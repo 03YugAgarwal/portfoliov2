01b04aa302714c4616a751ee206e1546ebd83a94
Yug <03yugagarwal@gmail.com>
2023-08-05 15:51:12 +0530

    home and nav fully responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,16 @@ export default function Home() {
       <h1 className="home-main">FRONT-END DEV.</h1>
       <h2 className="home-secondary">Crafting Seamless Web</h2>
       <h2 className="home-secondary">Experiences.</h2>
+      <button className="home-project-button">
+          {/* <SVG width="50px" height="60px" /> */}
+          <h3>Project</h3>
+        </button>
       <div className="home-bottom">
         <h3 className="home-h3">@freelancer</h3>
-        <button>
+        <button className="next-btn">
           <SVG width="50px" height="60px" />
         </button>
+        
       </div>
     </div>
   );
