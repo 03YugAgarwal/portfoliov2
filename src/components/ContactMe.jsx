e6ae853d148f8e30e39f6efa91fbac80c443e5e1
Yug <03yugagarwal@gmail.com>
2023-08-08 10:26:43 +0530

    contactme responsive

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -67,7 +67,6 @@ export default function ContactMe() {
             </ul>
           </div>
         </section>
-        <p className="contactme-copy">@copyright 2023</p>
       </div>
     </div>
   );
