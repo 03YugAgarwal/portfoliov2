54915c42381f05d532b0adc43a024e249fef514a
Yug <03yugagarwal@gmail.com>
2023-08-08 14:46:42 +0530

    projects - basic

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,38 @@
-const Projects = () => {
-  return (
-    <div>Projetcs</div>
-  )
-}
+import "./Projects.css";
+import { ReactComponent as SVG } from "../assets/arrow-right.svg";
+import { ReactComponent as SVG1 } from "../assets/scroll.svg";
+import { useNavigate } from "react-router-dom";
 
-export default Projects
+export default function Projects() {
+  const nav = useNavigate();
+  const handleHomeBtnClick = () => {
+    nav("/");
+  };
+  const handleContactBtnClick = () => {
+    nav("/contact");
+  };
+
+  return (
+    <div className="projects">
+      <h1 className="projects-main">Projects.</h1>
+      <h2 className="projects-secondary">
+        These are some of the projects I have worked on along with the tech
+        stacks I have used.
+      </h2>
+      <div className="projects-bottom">
+        <button className="projects-next-btn" onClick={handleHomeBtnClick}>
+          <SVG width="50px" height="60px" />
+        </button>
+        <button
+          className="projects-next-btn contact"
+          onClick={handleContactBtnClick}
+        >
+          <SVG width="50px" height="60px" />
+        </button>
+      </div>
+      <div className="projects-scroll-icon">
+        <SVG1 />
+      </div>
+    </div>
+  );
+}
