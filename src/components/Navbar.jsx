b97bfcb612beabd90983b5d0877a3279e2a9a15d
Yug <03yugagarwal@gmail.com>
2023-08-11 22:48:27 +0530

    Link update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,12 +23,12 @@ export default function Navbar() {
 
       <div className={`nav-links${hamburgerOpen?'-hamburger':''}`}>
 
-        <h2><Link to="resume">Resume</Link></h2>
+        <h2><a href="https://drive.google.com/file/d/1ej4ACqomuSdMh_IwcMAIceYiF6tyIjDi/view?usp=sharing" target="__blank">Resume</a></h2>
         <h2><a href="https://www.linkedin.com/in/yugagarwal03/" target="__blank">Linkedin</a></h2>
         {hamburgerOpen && <h2><Link to="/projects" onClick={() => setHamburgerOpen(!hamburgerOpen)}>Projects</Link></h2>}
         {<h2><a href="https://github.com/03YugAgarwal" target="__blank">Github</a></h2> && hamburgerOpen }
-        {hamburgerOpen &&  <h2><Link to="/contact" onClick={() => setHamburgerOpen(!hamburgerOpen)} >Contact Me.</Link></h2>}
-        <button onClick={() => nav('contact')}>Contact Me.</button>
+        {hamburgerOpen &&  <h2><Link to="/contact" onClick={() => setHamburgerOpen(!hamburgerOpen)} >Contact Me</Link></h2>}
+        <button onClick={() => nav('contact')}>Contact Me</button>
       </div>
     </div>
   );
