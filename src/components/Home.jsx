08b6f8057365f35414e0cbe1f5e10fd818e7ad49
Yug <03yugagarwal@gmail.com>
2023-09-29 13:42:56 +0530

    home page basic gsap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,23 +2,95 @@ import "./Home.css";
 import { ReactComponent as SVG } from "../assets/arrow-right.svg";
 import { useNavigate } from "react-router-dom";
 
+import { useRef, useEffect } from "react";
+
+import { gsap, Power3 } from "gsap";
+
 export default function Home() {
-  const nav = useNavigate()
+  const nav = useNavigate();
   const handleBtnClick = () => {
-    nav("projects")
+    nav("projects");
   };
+
+  let homeItem = useRef(null);
+  let homeItem_1 = useRef(null);
+  let homeItem_2 = useRef(null);
+  let homeItem_3 = useRef(null);
+  let homeItem_4 = useRef(null);
+
+  let t1 = gsap.timeline();
+  let ease = Power3.easeOut();
+
+  useEffect(() => {
+    t1.from(homeItem, 0.5, {
+      y: 75,
+      opacity: 0,
+      ease: ease,
+      delay: 0.5,
+    });
+    t1.from(homeItem_1, 0.5, {
+      y: 75,
+      opacity: 0,
+      ease: ease,
+    });
+    t1.from(homeItem_2, 0.5, {
+      y: 75,
+      opacity: 0,
+      ease: ease,
+    });
+    t1.from([homeItem_4,homeItem_3], 0.5, {
+      y: 75,
+      opacity: 0,
+      ease: ease,
+      stagger: 0.5
+    });
+  }, [ease, t1]);
 
   return (
     <div className="home">
-      <h1 className="home-main">FRONT-END DEV.</h1>
-      <h2 className="home-secondary">Crafting Seamless Web</h2>
-      <h2 className="home-secondary">Experiences.</h2>
-      <button className="home-project-button" onClick={handleBtnClick} >
+      <h1
+        className="home-main"
+        ref={(el) => {
+          homeItem = el;
+        }}
+      >
+        FRONT-END DEV.
+      </h1>
+      <h2
+        className="home-secondary"
+        ref={(el) => {
+          homeItem_1 = el;
+        }}
+      >
+        Crafting Seamless Web
+      </h2>
+      <h2
+        className="home-secondary"
+        ref={(el) => {
+          homeItem_2 = el;
+        }}
+      >
+        Experiences.
+      </h2>
+      <button
+        className="home-project-button"
+        ref={(el) => {
+          homeItem_4 = el;
+        }}
+        onClick={handleBtnClick}
+      >
         {/* <SVG width="50px" height="60px" /> */}
         <h3>Project</h3>
       </button>
       <div className="home-bottom">
-        <h3 className="home-h3">@freelancer</h3>
+        <h3
+          className="home-h3"
+          ref={(el) => {
+            homeItem_3 = el;
+          }}
+        >
+          @freelancer
+        </h3>
         <button className="next-btn" onClick={handleBtnClick}>
           <SVG width="50px" height="60px" />
         </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,75 @@
 import "./Navbar.css";
 import { Link, useNavigate } from "react-router-dom";
-import {useState} from 'react'
+import { useState } from "react";
 
 export default function Navbar() {
-
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
   const nav = useNavigate();
 
-
   return (
     <div className="nav">
-      <div className={`hamburger-menu${hamburgerOpen?'-open':''}`} onClick={()=> setHamburgerOpen(!hamburgerOpen)}>
+      <div
+        className={`hamburger-menu${hamburgerOpen ? "-open" : ""}`}
+        onClick={() => setHamburgerOpen(!hamburgerOpen)}
+      >
         <div className="hamburger-item"></div>
         <div className="hamburger-item"></div>
         <div className="hamburger-item"></div>
       </div>
-      <div className="nav-heading">
-        <h2 className={`nav-heading-h2${hamburgerOpen?'-open':''}`} onClick={() => nav('/')}>Yug Agarwal</h2>
+      <div
+        className="nav-heading"
+      >
+        <h2
+          className={`nav-heading-h2${hamburgerOpen ? "-open" : ""}`}
+          onClick={() => nav("/")}
+        >
+          Yug Agarwal
+        </h2>
       </div>
 
-
-      <div className={`nav-links${hamburgerOpen?'-hamburger':''}`}>
-
-        <h2><a href="https://drive.google.com/file/d/1ej4ACqomuSdMh_IwcMAIceYiF6tyIjDi/view?usp=sharing" target="__blank">Resume</a></h2>
-        <h2><a href="https://www.linkedin.com/in/yugagarwal03/" target="__blank">Linkedin</a></h2>
-        {hamburgerOpen && <h2><Link to="/projects" onClick={() => setHamburgerOpen(!hamburgerOpen)}>Projects</Link></h2>}
-        {<h2><a href="https://github.com/03YugAgarwal" target="__blank">Github</a></h2> && hamburgerOpen }
-        {hamburgerOpen &&  <h2><Link to="/contact" onClick={() => setHamburgerOpen(!hamburgerOpen)} >Contact Me</Link></h2>}
-        <button onClick={() => nav('contact')}>Contact Me</button>
+      <div className={`nav-links${hamburgerOpen ? "-hamburger" : ""}`}>
+        <h2>
+          <a
+            href="https://drive.google.com/file/d/1ej4ACqomuSdMh_IwcMAIceYiF6tyIjDi/view?usp=sharing"
+            target="__blank"
+          >
+            Resume
+          </a>
+        </h2>
+        <h2>
+          <a href="https://www.linkedin.com/in/yugagarwal03/" target="__blank">
+            Linkedin
+          </a>
+        </h2>
+        {hamburgerOpen && (
+          <h2>
+            <Link
+              to="/projects"
+              onClick={() => setHamburgerOpen(!hamburgerOpen)}
+            >
+              Projects
+            </Link>
+          </h2>
+        )}
+        {(
+          <h2>
+            <a href="https://github.com/03YugAgarwal" target="__blank">
+              Github
+            </a>
+          </h2>
+        ) && hamburgerOpen}
+        {hamburgerOpen && (
+          <h2>
+            <Link
+              to="/contact"
+              onClick={() => setHamburgerOpen(!hamburgerOpen)}
+            >
+              Contact Me
+            </Link>
+          </h2>
+        )}
+        <button onClick={() => nav("contact")}>Contact Me</button>
       </div>
     </div>
   );
